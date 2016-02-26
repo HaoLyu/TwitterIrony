d9d8678702edea4f7f2bf912cde1b173ef2e8657
HaoLyu <xalh8083@gmail.com>
2016-02-26 16:55:12 -0600

    fixed model

--- Classification/Model_historical_feature.py
+++ Classification/Model_historical_feature.py
@@ -8,9 +8,10 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import linear_model
 from sklearn.cross_validation import KFold
-from scipy.sparse import csr_matrix
 from sklearn.metrics import roc_auc_score
 from sklearn import grid_search
+from sklearn import cross_validation
+from scipy.sparse import csr_matrix
 
 # Generate the top 100 tfidf terms
 def Author_historical_salient_terms():
@@ -23,7 +24,8 @@ def Author_historical_salient_terms():
 	fit_tfidf_list = []
 	target_list = []
 	name_dict = {}
-	for i in range(dbtweets.find({'hist_list':{'$exists':True}}).count()): 
+	for i in xrange(dbtweets.find({'hist_list':{'$exists':True}}).count()): 
+	#for i in range(1000):
 		try:
 			author_full_name = (dbtweets.find({'hist_list':{'$exists':True}})[i]['author_full_name']).encode('utf-8')
 			tweetList = dbtweets.find({'hist_list':{'$exists':True}})[i]['hist_list']
@@ -59,10 +61,14 @@ def Author_historical_salient_terms():
 		except ValueError:
 			continue
 
+	client.close()
 	count_vect = CountVectorizer(vocabulary=tfidf_dict.keys())
+	del tfidf_dict
 	X = count_vect.fit_transform(fit_tfidf_list)
-	X = csr_matrix(X).toarray()
+	del fit_tfidf_list
+	#X = csr_matrix(X).toarray()
 	Y_target = np.array(target_list)
+	del target_list
 	print X.shape, Y_target.shape
 	name_list = name_dict.keys()
 	total_set_length = len(name_list)
@@ -76,20 +82,31 @@ def Author_historical_salient_terms():
 		train_index = np.array(temp_index)
 
 		temp_index = []
-		for i in test_index:
-			temp_index += name_dict[name_list[i]]
+		for j in test_index:
+			temp_index += name_dict[name_list[j]]
 		test_index = np.array(temp_index)
 
 		X_train, X_test = X[train_index], X[test_index]
 		Y_train, Y_test = Y_target[train_index], Y_target[test_index]
-		parameters = {'tol':[0.01,0.02,0.03,0.04,0.05], 'C':[1, 10]}
+		X_train_part, X_dev, Y_train_part, Y_dev = cross_validation.train_test_split(
+			X_train, Y_train, test_size = 0.11, random_state = 0)
+
+		parameters = {'tol':[0.01,0.1,0.001], 'C':[1, 10, 100]}
 		lr = linear_model.LogisticRegression(penalty='l2')
-		clf = grid_search.GridSearchCV(lr, parameters, cv=5)
-		clf.fit(X_train, Y_train)
-		y_true, y_pred = Y_test, clf.predict(X_test)
-		score = clf.score(X_test, Y_test)
+		clf = grid_search.GridSearchCV(lr, parameters)
+		clf.fit(X_dev, Y_dev)
+		best_params = clf.best_params_
+		del lr,clf
+		print best_params
+		tuned_clf = linear_model.LogisticRegression(penalty='l2', C=best_params['C'], tol=best_params['tol'])
+		tuned_clf.fit(X_train_part, Y_train_part)
+		y_true, y_pred = Y_test, tuned_clf.predict(X_test)
+		score = tuned_clf.score(X_test, Y_test)
+		del tuned_clf
+		print 'score is ',score
 		avg_score.append(score)
 		auc = roc_auc_score(y_true, y_pred)
+		print 'auc is', auc 
 		avg_auc_score.append(auc)
 
 	accuracy = reduce(lambda x, y: x + y, avg_score) / len(avg_score)
