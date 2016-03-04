61aa6f9d7a7921f6853b5175885bb91499b2438a
HaoLyu <xalh8083@gmail.com>
2016-03-04 02:22:59 -0600

    Browncluster

--- irony_package/Brown Cluster/BrownCluster.py
+++ irony_package/Brown Cluster/BrownCluster.py
@@ -5,114 +5,70 @@ import csv
 import nltk
 import pickle
 import sys
+import re
+from pymongo import MongoClient
+
 # Load the Twitter Word Clusters into a dictionary
-def get_cluster_dic(file):
+def get_cluster_dic(file="/Users/haolyu/TwitterIrony/irony_package/Brown Cluster/Twc.csv"):
 	f = open(file)
 	cluster_dic = {}
+	cluster1000 = {}
 	csv_f = csv.reader(f, delimiter='\t')
 	for row in csv_f:
-		try:
+		if row[0] not in cluster1000:
+			cluster1000[row[0]] = 1
+		if row[1] in cluster_dic:
 			cluster_dic[row[1]].append(row[0])
-		except KeyError:
-			cluster_dic[row[1]] = [row[0]]
-		except IndexError:
-			break
-
-	return cluster_dic
-
-cluster_dic = get_cluster_dic("Twc.csv")
-
-# Get 1000 clusters' names, file is the name of corpus
-def get_cluster_name(file):
-	f = open(file)
-	cluster = []
-	csv_f = csv.reader(f, delimiter='\t')	
-	for row in csv_f:
-		if row[0] in cluster:
-			continue
 		else:
-			cluster.append(row[0])
+			cluster_dic[row[1]] = row[0]
 
-	return cluster
+	return (cluster_dic,cluster1000)
+
+cluster_dic = get_cluster_dic("/Users/haolyu/TwitterIrony/irony_package/Brown Cluster/Twc.csv")[0]
+
 
 # Given a sentence then tokenizer it into list of tokens, return a list of tuples of
-# tokens which are in the Brown Cluster and a count of '1'.
-def Map(L):
-	L = nltk.word_tokenize(L)
-	print "Sentence Tokenizers:", L
+# tokens which are in the Brown Cluster 
+def Map(text):
+	global cluster_dic
+	text = re.sub(r"(?:\@|https?\://)\S+", "", text)
+	pat = re.compile(r'\s+')
+	text = pat.sub(' ', text).strip()
+	L = nltk.word_tokenize(text)
+	L = [w.lower() for w in L]
 	results = []
 	for word in L:
-		if(cluster_dic.get(word)):
-			results.append ((cluster_dic[word], 1))
+		if word in cluster_dic:
+			results.append(cluster_dic[word])
 
 	return results
 
-"""
-Group the sublists of (token, 1) pairs into a term-frequency-list
-map, so that the Reduce operation later can work on sorted
-term counts. The returned result is a dictionary with the structure
-{token : [([token], 1), ...] .. }
-"""
-def Partition(L):
-
-  	tf = {}
-  	for p in L:
-		try:
-	    		tf[p[0][0]].append(p)
-	  	except KeyError:
-	    		tf[p[0][0]] = [p]
-
-  	return tf
 
 
-"""
-Given a (token, [([token], 1) ...]) tuple, collapse all the
-count tuples from the Map operation into a single term frequency
-number for this token, and return a list of final tuple [(token, frequency),...].
-"""
-def Reduce(Mapping):
-  return (Mapping[0], sum(pair[1] for pair in Mapping[1]))
 
 
 if __name__ == '__main__':
-	cluster_namelist = get_cluster_name('Twc.csv')
-	try:
-		arg1 = sys.argv[1]
-		arg2 = sys.argv[2]
-	except IndexError:
-		print "You need to type input name and output name"
-		sys.exit(1)
-
-	outputlist = []
-	inputfile = open(arg1, 'r')
-	outputfile = open(arg2, 'w')
-	count = 0
-# Read the sentences in inputfile and process them by Brown Clustering
-# Load the resultlist into outputfile and display first 3 results
-	for sentence in inputfile.readlines():
-		count = count + 1
-		sentence_map = Map(sentence)
-		sentence_reduce = map(Reduce, Partition(sentence_map).items()) 
-		end_dic = {}
-		end_list = []
+	client = MongoClient('127.0.0.1', 27017)
+	db = client['IronyHQ']
+	dbtweets = db.tweets
+	number = dbtweets.find({'BrownCluster':{'$exists':False}}).count()
+	for i in range(number):
+		#try:
+		tweet_id = dbtweets.find({'BrownCluster':{'$exists':False}})[0]['tweet_id']
+		print tweet_id
+		text = dbtweets.find({'BrownCluster':{'$exists':False}})[0]['tweet_text']
+		BrownCluster = Map(text)
 		
-		for cluster in cluster_namelist:
-			end_dic[cluster] = 0
-
-		for one in sentence_reduce:
-			end_dic[one[0]] = one[1]
-
-		for cluster in cluster_namelist:
-			end_list.append(end_dic[cluster])
-
-		if(count < 4):
-			print "result", count, ":", sentence_reduce
-			print "-"*50
-		outputlist.append(end_list)
-
-	pickle.dump(outputlist, outputfile)
-	# To read it back: outputlist = pickle.load(outputfile)
-	inputfile.close()
-	outputfile.close()
-
+		result = dbtweets.update_one({"tweet_id": tweet_id},
+			{
+			    "$set": {
+	                "BrownCluster":BrownCluster
+	        	}
+			}
+		)
+		
+		#except Exception:
+			#print i
+			#continue
+			
 
--- irony_package/fix_data.py
+++ irony_package/fix_data.py
@@ -14,7 +14,9 @@ def remove_sarcasm_tag():
 		tweet_id = dbtweets.find()[i]['tweet_id']
 		tweet_text = dbtweets.find()[i]['tweet_text']
 		tweet_text = tweet_text.replace('#sarcasm','').strip()
+		tweet_text = tweet_text.replace('#Sarcasm','').strip()
 		tweet_text = tweet_text.replace('#sarcastic','').strip()
+		tweet_text = tweet_text.replace('#Sarcastic','').strip()
 		result = dbtweets.update_one({"tweet_id": tweet_id},
 				{
 				    "$set": {
@@ -22,47 +24,32 @@ def remove_sarcasm_tag():
 		        	}
 				}
 			)
-def check_sarcasm_tag():
+def check_intensifier_tag():
 	# Connect to MongoDB
 	client = MongoClient('127.0.0.1', 27017)
 	db = client['IronyHQ']
 	dbtweets = db.tweets
-
-	all_unigrams = BOW.Get_unigrams_bigrams(AllT.collect_text())[0]
-	print type(all_unigrams)
+	d = {}
 	
-	print all_unigrams
-	print '#sarcasm' in all_unigrams
-	print '#sarcastic' in all_unigrams
-	print 'sarcasm' in all_unigrams
-	print 'sarcastic' in all_unigrams
-	if 'sarcasm' in all_unigrams:
-		print all_unigrams.index('sarcasm')
-	if 'sarcastic' in all_unigrams:
-		print all_unigrams.index('sarcastic')
-	vect1 = CountVectorizer(vocabulary=all_unigrams)
 	
-	"""
-	for i in range(dbtweets.find().count()):
-		tweet_id = dbtweets.find()[i]['tweet_id']
-		tweet_text = dbtweets.find()[i]['tweet_text']
-		if i%200 == 0:
-			print i
-		c = 'sarcasm'
-		d= 'sarcastic'
-		a = '#sarcasm'
-		b = '#sarcastic'
-		if (a in tweet_text) or (b in tweet_text) or(c in tweet_text) or(d in tweet_text):
-			print tweet_id, tweet_text
+	for i in range(dbtweets.find({'intensifier':{'$exists':True}}).count()):
+		intensifier = dbtweets.find({'intensifier':{'$exists':True}})[i]['intensifier']
 		
-	"""
+		if intensifier in d:
+			continue
+		else:
+			d[intensifier] = 1
+
+	print d
+		
+	
 
 if __name__ == '__main__':
 	try:
 		if sys.argv[1] == 'remove_sarcasm_tag':
 			remove_sarcasm_tag()
 		elif sys.argv[1] == 'check':
-			check_sarcasm_tag()
+			check_intensifier_tag()
 
 		else:
 			print 'other mode'
